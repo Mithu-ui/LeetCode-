3527caab28915cb7c4c7c49478e4b5ab638f327d
Mithu-ui <23AK1A3061@aits-tpt.edu.in>
2026-03-13 10:31:37 +0530

    Time: 0 ms (100%), Space: 42 MB (70.31%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,11 +1,23 @@
 class Solution {
+    static int dp[];
+    // private int climb(int n){
+    //     if(n==1) return 1;
+    //     if(n==2) return 2;
+    //     if(dp[n-1]!=-1) return dp[n-1];
+    //     return dp[n-1]=climb(n-1)+climb(n-2);
+    // }
     public int climbStairs(int n) {
-        int a=1,b=1,temp=0;
-        for(int i=0;i<n;i++){
-            temp=a+b;
-            a=b;
-            b=temp;
+        // dp=new int[n];
+        // Arrays.fill(dp,-1);
+        // return climb(n);
+        if(n==1) return 1 ;
+        if(n==2) return 2;
+        int[] dp=new int[n];
+        dp[0]=1;
+        dp[1]=2;
+        for(int i=2;i<n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        return a;
+        return dp[n-1];
     }
 }
