900961a157e88c1fe9002161ea236a57f235695e
Mithu-ui <23AK1A3061@aits-tpt.edu.in>
2026-07-13 21:09:09 +0530

    Time: 0 ms (100%), Space: 44.5 MB (92.55%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,18 +1,14 @@
 class Solution {
-    private int prev;
-    private int BinarySearch(int []nums,int target,int low,int high){
-        int mid=(low+high)/2;
+    private int helper(int []arr,int target, int low,int high){
         if(low<=high){
-            if(nums[mid]==target) return mid;
-            if(nums[mid]<target) return BinarySearch(nums,target,mid+1,high);
-            if(nums[mid]>target)  return BinarySearch(nums,target,low,mid-1);
+            int mid=(low+high)/2;
+            if(arr[mid]==target) return mid;
+            if(arr[mid]<target) return helper(arr,target,mid+1,high);
+            if(arr[mid]>target) return helper(arr,target,low,mid-1);
         }
         return low;
     }
     public int searchInsert(int[] nums, int target) {
-        int n=nums.length;
-        if(nums[n-1]<target) return n;
-        if(nums[0]>target) return 0;
-        return BinarySearch(nums,target,0,n-1);
+        return helper(nums,target,0,nums.length-1);
     }
 }
